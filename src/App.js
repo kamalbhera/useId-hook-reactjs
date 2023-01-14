d60df203d83bbb447a77f1da08218cb192434096
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-14 17:30:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React, {useId} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const id = useId();
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +17,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - {id}
         </a>
+        <form>
+          <label htmlFor={`email-${id}`}>Email</label>
+          <input type="text" id={`email-${id}`} name="email" />
+
+          <label htmlFor={`password-${id}`}>Password</label>
+          <input type="password" id={`password-${id}`} name="password" />
+        </form>
       </header>
     </div>
   );
